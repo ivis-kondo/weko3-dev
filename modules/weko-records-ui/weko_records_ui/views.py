fe30e963646279dfee0506767855114e19a9bf8b
ivis-kondo <hiroaki.kondo@ivis.co.jp>
2025-11-25 17:40:10 +0900

    fix s3 compatible service copy and location admin page permissions

--- modules/weko-records-ui/weko_records_ui/views.py
+++ modules/weko-records-ui/weko_records_ui/views.py
@@ -1495,6 +1495,7 @@ def copy_bucket():
         return jsonify(uri)
     except Exception as e:
         current_app.logger.error(str(e))
+        traceback.print_exc()
         return jsonify({'error': str(e)}), 400
 
 
